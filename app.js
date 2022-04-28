f2887c620333bfcb6e066333cc4a53d74b3fb21c
ManavGhaiCode <manavghai12october@gmail.com>
2022-04-28 09:47:37 +0530

    add base ejs and css

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 app.listen(3000, () => {
-    console.log('Sever servering at port 3000');
+    console.log('Server servering at port 3000');
 })
