cdb96580cbb1f4f50f3b3847fc9251b5e3676878
ManavGhaiCode <manavghai12october@gmail.com>
2022-05-03 09:57:30 +0530

    add send score to sever

--- app.js
+++ app.js
@@ -9,9 +9,15 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.render('home', { words: getRandSpellings(dictionary) });
+})
+
+app.post('/save-score', (req, res) => {
+    console.log(req.body);
+    res.send('done');
 })
 
 app.listen(3000, () => {
--- public/app.js
+++ public/app.js
@@ -1,19 +1,17 @@
-(async function() {
+(async function code() {
     const currentSpelling = document.querySelector('#current-spelling');
     const rw = document.querySelector('#rw');
+    const score = document.querySelector('#score');
     const input = document.querySelector('input[type=text]');
     
     let currentWordIndex = 0;
-    let score = 0;
+    let points = 0;
     
     const sleep = sec => {
         return new Promise(res => setTimeout(res, (sec * 1000)));
     }
 
     const spellCheak = (el, spelling) => {
-        console.log(el.value);
-        console.log(spelling);
-
         if (el.value != spelling.trim()) {
             return false;
         } else {
@@ -24,7 +22,7 @@
     input.addEventListener('keydown',async e => {
         if (e.key === 'Enter') {
             if (spellCheak(input, words[currentWordIndex]) === true) {
-                score++;
+                points++;
 
                 input.classList.add('hidden');
                 
@@ -35,6 +33,9 @@
 
                 await sleep(1);
                 rw.classList.add('hidden');
+
+                currentSpelling.classList.remove('hidden');
+                currentSpelling.textContent = `Learn this spelling: ${words[currentWordIndex]}`;
             } else {
                 input.classList.add('hidden');
                 
@@ -45,33 +46,45 @@
 
                 await sleep(1);
                 rw.classList.add('hidden');
+
+                currentSpelling.classList.remove('hidden');
+                currentSpelling.textContent = `Learn this spelling: ${words[currentWordIndex]}`;
             }
         }
     })
 
     const main = async () => {
         for (word in words) {
-            if (currentWordIndex === 0) {
-                await sleep(10);
+            await sleep(10);
 
-                currentSpelling.classList.add('hidden');
-                input.classList.remove('hidden');
-            } else {
-                currentSpelling.textContent = `Learn this spelling: ${words[currentWordIndex]}`;
-
-                currentSpelling = document.querySelector('#current-spelling');
-                currentSpelling.textContent = word[currentWordIndex];
-
-                console.log(words[currentWordIndex]);
-
-                currentSpelling.classList.add('hidden');
-
-                await sleep(10);
-
-                currentSpelling.classList.add('hidden');
-                input.classList.remove('hidden');
-            }
+            currentSpelling.classList.add('hidden');
+            input.classList.remove('hidden');
         }
+
+        currentSpelling.classList.add('hidden');
+        input.classList.add('hidden');
+        rw.classList.add('hidden');
+    
+        score.textContent = `Your score is: ${points}`;
+    
+        score.classList.remove('hidden');
+    
+        const data = {
+            points,
+            pointsPercentage: `${((points / 10) * 100)}%`
+        }
+    
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+    
+        console.log(JSON.stringify(data))
+    
+        fetch('/save-score', options);
     }
 
     main();
